a8135b466adc7b87929069d0b04a0e9a7ee7c37f
Rajvardhan <singhrajvardhan2003@gmail.com>
2023-05-17 13:04:13 +0530

    leetcode problem 1281

---
+++ 1281.cpp
@@ -0,0 +1,19 @@
+// 1281. Subtract the Product and Sum of Digits of an Integer
+
+class Solution {
+    public int subtractProductAndSum(int n) {
+        int prod = 1;
+        int sum = 0;
+
+        while(n!=0){
+            int digit = n%10;
+            prod = prod * digit;
+            sum = sum + digit;
+
+            n = n/10;
+        }
+
+        int answer = prod-sum;
+        return answer;
+    }
+}
